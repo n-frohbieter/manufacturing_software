99bdff1628e0d1b9039ade885991d2cba75c04e0
Nathan Frohbieter <nfrohbieter@gmail.com>
2026-03-21 22:43:51 -0500

    created  methods in inventory_item

--- inventory/inventory_item.py
+++ inventory/inventory_item.py
@@ -8,6 +8,26 @@ class InventoryItem:
         self.price_per_unit = price_per_unit
         self.reorder_lead_time = reorder_lead_time
         self.total_cost = total_cost
-        self.location
+        #self.location
 
-    
+    def change_description(self, new_description):
+        self.description = new_description
+
+    def increase_total_amount(self, amount):
+        self.total_amount += amount
+
+    def decrease_total_amount(self, amount):
+        self.total_amount -= amount
+
+    def change_reorder_amount(self, new_reorder_amount):
+        self.reorder_amount = new_reorder_amount
+
+    def change_price_per_unit(self, new_price_per_unit):
+        self.price_per_unit = new_price_per_unit
+
+    def change_reorder_lead_time(self, new_reorder_lead_time):
+        self.reorder_lead_time = new_reorder_lead_time
+
+    def get_total_cost(self):
+        return self.price_per_unit * self.total_amount
+
